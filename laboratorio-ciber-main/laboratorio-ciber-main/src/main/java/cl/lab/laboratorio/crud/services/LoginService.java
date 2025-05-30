fa0db066b99061e16ee648a5b346bbab8a19bf27
Nicolás Sánchez <nicolas.sanchez.al@usach.cl>
2025-05-30 17:20:53 -0400

    lab 2 fix

--- laboratorio-ciber-main/laboratorio-ciber-main/src/main/java/cl/lab/laboratorio/crud/services/LoginService.java
+++
@@ -1,9 +0,0 @@
-package cl.lab.laboratorio.crud.services;
-
-import cl.lab.laboratorio.crud.model.UsuarioModel;
-
-public interface LoginService {
-
-    UsuarioModel login(String usuario, String password);
-
-}
